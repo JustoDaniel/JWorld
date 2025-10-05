a504427309f0e5eda93b6a3c992ed2a01b05dd86
Justo Daniel <carlosajdaniel@gmail.com>
2025-10-05 18:59:37 -0300

    vou atualizar para ver se configo mudar a maquina

--- GCP/CriacaodeSnapshot/backend.tf
+++ GCP/CriacaodeSnapshot/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "gcs" {
     bucket = "tf-state-justo"                  # <-- MUDE AQUI para o nome do bucket que você criou
-    prefix = "tf-criacaodesnapshot" # (Opcional) Uma pasta dentro do bucket para organizar o estado
+    prefix = "tf-criacaodesnapshot-srv01" # (Opcional) Uma pasta dentro do bucket para organizar o estado
   }
 
 
